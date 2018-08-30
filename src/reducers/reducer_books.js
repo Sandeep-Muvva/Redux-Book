92a0439637cd2a961d96324bcf9e15702f08dffc
Sandeep <muvva@usc.edu>
2018-08-29 20:09:05 -0500

    Initial Commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function (){
+  return [
+    {title:'Javascript',pages:101},
+    {title:'Harry Potter',pages:39},
+    {title:'The Dark Tower',pages:22},
+    {title:'Eloquent Ruby',pages:1}
+  ]
+}
